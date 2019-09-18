b69cf3bf23301aea9f10e5d9b7f5428c1590c836
xzbd <xzbd745114565@163.com>
2019-09-18 10:51:48 +0800

    Initial commit

---
+++ src/main/java/com/quming/QumingQuickApplication.java
@@ -0,0 +1,13 @@
+package com.quming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QumingQuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QumingQuickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quming/QumingQuickApplicationTests.java
@@ -0,0 +1,16 @@
+package com.quming;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QumingQuickApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
