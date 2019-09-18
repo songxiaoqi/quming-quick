5787655e2d9531048261fc451756f40c9eca2690
xzbd <xzbd745114565@163.com>
2019-09-18 17:42:36 +0800

    日志

--- src/test/java/com/quming/QumingQuickApplicationTests.java
+++ src/test/java/com/quming/QumingQuickApplicationTests.java
@@ -4,6 +4,8 @@ import com.quming.entity.Order;
 import com.quming.service.OrderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,6 +16,8 @@ import java.util.List;
 @SpringBootTest
 public class QumingQuickApplicationTests {
 
+    Logger logger = LoggerFactory.getLogger(QumingQuickApplicationTests.class);
+
     @Autowired
     OrderService orderService;
 
@@ -21,7 +25,7 @@ public class QumingQuickApplicationTests {
     public void contextLoads() {
         List<Order> orders = orderService.selectAllOrder();
         orders.forEach(order -> {
-            System.out.println(String.format("订单id：%d,订单状态：%d",order.getOrderId(),order.getOrderState()));
+            logger.info(String.format("订单id：%d,订单状态：%d",order.getOrderId(),order.getOrderState()));
         });
     }
 
