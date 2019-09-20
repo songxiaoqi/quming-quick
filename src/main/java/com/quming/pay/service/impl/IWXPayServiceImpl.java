05cfa4e96c85693df26a81b96a75f79191c18c39
xzbd <xzbd745114565@163.com>
2019-09-20 14:34:47 +0800

    支付

---
+++ src/main/java/com/quming/pay/service/impl/IWXPayServiceImpl.java
@@ -0,0 +1,8 @@
+package com.quming.pay.service.impl;
+
+import com.quming.pay.service.IWXPayService;
+import org.springframework.stereotype.Service;
+
+@Service("iWXPayService")
+public class IWXPayServiceImpl implements IWXPayService {
+}
